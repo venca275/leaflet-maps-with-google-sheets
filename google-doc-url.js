e1f43087eacef483d217eb8c240f1f46b4a485fe
venca275 <93138075+venca275@users.noreply.github.com>
2023-04-14 16:14:26 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1c9VasB0ZrWZFAMXr2GA2NSkrbfV59GePPCsQAqYHK9w/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAocQK6_JkwYwgIQbzrv08iyZKL4yEF_3M';
